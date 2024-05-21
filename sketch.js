d9d6fc40e7bc95900b490e18e63c9de279fc9a0c
maryferreir <0000110801396xsp@al.educacao.sp.gov.br>
2024-05-21 16:36:34 -0300

    alterei código

--- sketch.js
+++ sketch.js
@@ -1,31 +1,15 @@
-let cor;
-let circuloX;
-let circuloY;
-
 function setup() {
-  createCanvas(400, 400);
-  background(color(100, 0, 0));
-  cor = "color(random(0, 255), random(0, 255), random(0,255))";
-  circuloX = [0, 0, 0];
-  circuloY = [random(height), random(height), random(height)];
+  createCanvas(600, 600);
+  background("black");
 }
+
 function draw() {
-  fill(cor);
-
-  // console.log(circuloX length);
-  for (let contador in circuloX) {
-    console.log(contador);
-    circle(circuloX[contador], circuloY[contador], 50);
-    circuloX[contador] += random(0, 3);
-    circuloY[contador] += random(-3, 3);
-
-    if (circuloX[contador] >= width) {
-      circuloX[contador] = 0;
-      circuloY[contador] = random(height);
-    }
+  stroke("blue");
+  fill("red");
+  
+  if(mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
   }
 
-  if (mouseIsPressed) {
-    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
   }
-}
+  
